377724e556406d893a1513c74a9ffb59d860e92d
YutoFunaki <kaizuka0629@gmail.com>
2023-02-22 20:31:00 +0900

    テンプレート文字列

--- sample.js/sample.js
+++ sample.js/sample.js
@@ -2,4 +2,15 @@
 
 //varは再宣言可能な変数
 //letは再宣言不可な変数
-//constは定数
+//constはオブジェクト,配列以外は上書き不可だが再宣言可能
+
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";//
+// val5.push("monkey");//push：配列に追加
+// console.log(val5);
+
+//テンプレート文字列
+// const named = "named";
+// const age = 28;
+// const message1 = `私の名前は${named}です。年齢は${age}です。`;
+// console.log(message1);
