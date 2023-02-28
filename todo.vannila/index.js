71829b8a71eccb5b78ec5c33d88977a7c8c06422
YutoFunaki <kaizuka0629@gmail.com>
2023-02-28 17:45:18 +0900

    liタグ作成

--- todo.vannila/index.js
+++ todo.vannila/index.js
@@ -1,13 +1,18 @@
 
 const onClickAdd = () => {
   //テキストボックスの値を取得し初期化する処理
+  //https://ywork2020.com/content/js-value.html これ見たらgetElementById.valueがわかる
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
   //div生成
   const div = document.createElement("div");
+  div.className = "list-row";
 
-  console.log(div);
+  //luタグ作成
+  const li = document.createElement("li");
+  //innerTextは要素のなかに何を入れるかを決めることができる。今回は入力された値のinputTextを入れる。
+  li.innerText = inputText;
 }
 
 document
